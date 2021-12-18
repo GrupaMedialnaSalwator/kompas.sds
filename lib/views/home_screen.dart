1a2c94b35cd681185c80bc5d5933b1dab13e0350
Łukasz Darowski <besaintsds@gmail.com>
2021-12-18 14:48:54 +0100

    fix overflow in home screen

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -71,105 +71,108 @@ class HomePage extends StatelessWidget {
         ),
         body: TabBarView(
           children: [
-            Column(
-              children: [
-                // Container(
-                //   alignment: Alignment.topLeft,
-                //   margin: EdgeInsets.fromLTRB(36, 36, 0, 72),
-                //   child: Text(
-                //     "Witamy w Bagnie",
-                //     style: Theme.of(context).textTheme.headline5,
-                //   ),
-                // ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  margin: EdgeInsets.fromLTRB(36, 36, 0, 16),
-                  child: Text(
-                    "Zapraszamy",
-                    style: Theme.of(context).textTheme.headline1,
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  // Container(
+                  //   alignment: Alignment.topLeft,
+                  //   margin: EdgeInsets.fromLTRB(36, 36, 0, 72),
+                  //   child: Text(
+                  //     "Witamy w Bagnie",
+                  //     style: Theme.of(context).textTheme.headline5,
+                  //   ),
+                  // ),
+                  Container(
+                    alignment: Alignment.topLeft,
+                    margin: EdgeInsets.fromLTRB(36, 36, 0, 16),
+                    child: Text(
+                      "Zapraszamy",
+                      style: Theme.of(context).textTheme.headline1,
+                    ),
                   ),
-                ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  margin: EdgeInsets.fromLTRB(36, 0, 0, 4),
-                  child: Text(
-                    "do przygody",
-                    style: Theme.of(context).textTheme.headline2,
+                  Container(
+                    alignment: Alignment.topLeft,
+                    margin: EdgeInsets.fromLTRB(36, 0, 0, 4),
+                    child: Text(
+                      "do przygody",
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
                   ),
-                ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  margin: EdgeInsets.fromLTRB(36, 0, 0, 18),
-                  child: Text(
-                    "w naszym klasztorze",
-                    style: Theme.of(context).textTheme.headline2,
+                  Container(
+                    alignment: Alignment.topLeft,
+                    margin: EdgeInsets.fromLTRB(36, 0, 0, 18),
+                    child: Text(
+                      "w naszym klasztorze",
+                      style: Theme.of(context).textTheme.headline2,
+                    ),
                   ),
-                ),
-                Container(
-                  height: 350,
-                  //width: 250,
-                  child: ListView.separated(
-                    padding: EdgeInsets.all(20),
-                    itemCount: 3,
-                    separatorBuilder: (BuildContext context, int index) =>
-                        VerticalDivider(
-                            width: 20, color: AppColors.primaryWhite),
-                    itemBuilder: (BuildContext context, int index) {
-                      return Card(
-                        semanticContainer: true,
-                        clipBehavior: Clip.antiAliasWithSaveLayer,
-                        child: InkWell(
-                          onTap: () {
-                            Get.bottomSheet(
-                              SelectWidget(args: Arguments(index)),
-                              isScrollControlled: true,
-                            );
+                  Container(
+                    height: 350,
+                    //width: 250,
+                    child: ListView.separated(
+                      padding: EdgeInsets.all(20),
+                      itemCount: 3,
+                      separatorBuilder: (BuildContext context, int index) =>
+                          VerticalDivider(
+                              width: 20, color: AppColors.primaryWhite),
+                      itemBuilder: (BuildContext context, int index) {
+                        return Card(
+                          semanticContainer: true,
+                          clipBehavior: Clip.antiAliasWithSaveLayer,
+                          child: InkWell(
+                            onTap: () {
+                              Get.bottomSheet(
+                                SelectWidget(args: Arguments(index)),
+                                isScrollControlled: true,
+                              );
 
-// Get.toNamed(AppRoutes.select,
-//                               arguments: Arguments(index)),
-                          },
-                          child: Stack(
-                            children: [
-                              Container(
-                                height: 350,
-                                width: 250,
-                                padding: EdgeInsets.all(16.0),
-                                alignment: Alignment.bottomLeft,
-                                decoration: BoxDecoration(
-                                  image: DecorationImage(
-                                    image: AssetImage(tripAssets[index]),
-                                    fit: BoxFit.fitWidth, //DecorationImage
+                              // Get.toNamed(AppRoutes.select,
+                              //                               arguments: Arguments(index)),
+                            },
+                            child: Stack(
+                              children: [
+                                Container(
+                                  height: 350,
+                                  width: 250,
+                                  padding: EdgeInsets.all(16.0),
+                                  alignment: Alignment.bottomLeft,
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: AssetImage(tripAssets[index]),
+                                      fit: BoxFit.fitWidth, //DecorationImage
+                                    ),
+                                  ), //BoxDecoration
+
+                                  child: Text(
+                                    tripTitles[index],
+                                    //style: Theme.of(context).textTheme.headline2,
+                                    style: Theme.of(context)
+                                        .textTheme
+                                        .headline3
+                                        ?.copyWith(
+                                            color: AppColors.primaryWhite),
                                   ),
-                                ), //BoxDecoration
+                                ), //Text
 
-                                child: Text(
-                                  tripTitles[index],
-                                  //style: Theme.of(context).textTheme.headline2,
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .headline3
-                                      ?.copyWith(color: AppColors.primaryWhite),
-                                ),
-                              ), //Text
-
-                              // Image.asset(
-                              //   tripAssets[index],
-                              //   fit: BoxFit.fitWidth,
-                              // ),
-                            ],
+                                // Image.asset(
+                                //   tripAssets[index],
+                                //   fit: BoxFit.fitWidth,
+                                // ),
+                              ],
+                            ),
                           ),
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20.0),
-                        ),
-                        elevation: 8,
-                        margin: EdgeInsets.all(4),
-                      );
-                    },
-                    scrollDirection: Axis.horizontal,
-                  ),
-                )
-              ],
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          elevation: 8,
+                          margin: EdgeInsets.all(4),
+                        );
+                      },
+                      scrollDirection: Axis.horizontal,
+                    ),
+                  )
+                ],
+              ),
             ),
             Center(child: Text("This tab shows a Map View")),
             Center(child: Text("This tab shows a Help View")),
