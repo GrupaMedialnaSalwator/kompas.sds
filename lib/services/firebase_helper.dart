00076f5a5bbcc96d4e750b59b555e1f7ec0f1e0b
Michal Knut <knut.michal@gmail.com>
2022-01-09 15:30:59 +0100

    finalized integration

---
+++ lib/services/firebase_helper.dart
@@ -0,0 +1,15 @@
+//TODO: This is code helps to check Firebase connection during the development phase and should be removed in the future.
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+CollectionReference _collectionRef =
+    FirebaseFirestore.instance.collection('collection');
+
+Future<void> getData() async {
+  // Get docs from collection reference
+  QuerySnapshot querySnapshot = await _collectionRef.get();
+
+  // Get data from docs and convert map to List
+  final allData = querySnapshot.docs.map((doc) => doc.data()).toList();
+
+  print(allData);
+}
