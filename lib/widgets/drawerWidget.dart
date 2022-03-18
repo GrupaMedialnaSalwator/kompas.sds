5e6fc8659833235b9e4c55ae4a3450860d6c4563
Michal Knut <knut.michal@gmail.com>
2022-03-18 19:18:46 +0100

    refined drawer, added team img to info

--- lib/widgets/drawerWidget.dart
+++ lib/widgets/drawerWidget.dart
@@ -1,11 +1,13 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:kompas/statics/colors.dart';
 import 'package:kompas/statics/constants.dart';
 import 'package:kompas/statics/kompasText.dart';
 import 'package:kompas/statics/text_styles.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DrawerWidget extends StatelessWidget {
-  final padding = EdgeInsets.symmetric(horizontal: 20);
+  final padding = EdgeInsets.symmetric(horizontal: Constants.buttonMargin);
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -17,44 +19,71 @@ class DrawerWidget extends StatelessWidget {
               padding: padding,
               child: Column(
                 children: [
-                  const SizedBox(height: 24),
+                  const SizedBox(height: Constants.bottomMargin),
                   Image.asset(
                       "assets/images/icons/wsd_logo_white_transparent.png",
-                      scale: 7,
+                      scale: Constants.drawerLogoScale,
                       fit: BoxFit.fitWidth),
-                  const SizedBox(height: 24),
+                  const SizedBox(height: Constants.bottomMargin),
                   KompasText(
                       style: AppTextStyles.headerH3White, text: "kompas.sds"),
                   KompasText(
                       style: AppTextStyles.paragraphTextWhite,
                       text: "wersja: 0.0.1 (alpha)"),
-                  const SizedBox(height: 24),
+                  const SizedBox(height: Constants.bottomMargin),
                   ClipRRect(
-                    child: Image.asset("assets/images/gms_team.png"),
-                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    child: Image.asset("assets/images/gms_team_white.png"),
+                    borderRadius: BorderRadius.all(Radius.circular(Constants.borderRadius)),
                   ),
-                  const SizedBox(height: 24),
+                  const SizedBox(height: Constants.bottomMargin),
                   KompasText(
                       style: AppTextStyles.paragraphTextWhite,
+                      textAlign: TextAlign.center,
                       text:
-                          "Grupa Medialna Salwator życzy udanego pobytu w Bagnie!"),
-                  const SizedBox(height: 24),
-                  Padding(
-                    padding: const EdgeInsets.all(Constants.minMargin),
-                    child: TextButton(
-                      child: Text("Oceń nas"),
-                      style: ButtonStyle(
-                          shape:
-                              MaterialStateProperty.all<RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                        borderRadius:
-                            BorderRadius.circular(Constants.borderRadius),
-                      ))),
-                      onPressed: () {
-                        print('will open app rating system in the future here');
-                      },
+                          "Grupa Medialna Salwator życzy dobrego pobytu w Bagnie!"),
+                  const SizedBox(height: Constants.spacerLarge),
+                  KompasText(
+                      style: AppTextStyles.paragraphTextWhite,
+                      textAlign: TextAlign.center,
+                      text:
+                          "Wyższe Seminarium Duchowne Salwatorianów w Bagnie"),
+                  const SizedBox(height: Constants.minMargin),
+                  Container(
+                    margin: EdgeInsets.symmetric(
+                        horizontal: Constants.insideMargin),
+                    child: RichText(
+                      text: TextSpan(
+                        children: [
+                          TextSpan(
+                            text: 'www.wsd.sds.pl',
+                            style: TextStyle(
+                                color: AppColors.primaryWhite,
+                                decoration: TextDecoration.underline),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                launch('https://wsd.sds.pl/');
+                              },
+                          ),
+                        ],
+                      ),
                     ),
                   ),
+                  // Padding(
+                  //   padding: const EdgeInsets.all(Constants.minMargin),
+                  //   child: TextButton(
+                  //     child: Text("Oceń nas"),
+                  //     style: ButtonStyle(
+                  //         shape:
+                  //             MaterialStateProperty.all<RoundedRectangleBorder>(
+                  //                 RoundedRectangleBorder(
+                  //       borderRadius:
+                  //           BorderRadius.circular(Constants.borderRadius),
+                  //     ))),
+                  //     onPressed: () {
+                  //       print('will open app rating system in the future here');
+                  //     },
+                  //   ),
+                  // ),
                 ],
               ),
             ),
