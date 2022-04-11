01b86e3f31f2d8e9fb996a0ab4ef28ff8d6eee10
Łukasz Darowski <lukasz.darowski@gmail.com>
2022-04-11 12:43:52 +0200

    add review us button

--- lib/widgets/drawerWidget.dart
+++ lib/widgets/drawerWidget.dart
@@ -4,6 +4,7 @@ import 'package:kompas/statics/colors.dart';
 import 'package:kompas/statics/constants.dart';
 import 'package:kompas/statics/kompasText.dart';
 import 'package:kompas/statics/text_styles.dart';
+import 'package:launch_review/launch_review.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class DrawerWidget extends StatelessWidget {
@@ -22,59 +23,44 @@ class DrawerWidget extends StatelessWidget {
               Column(
                 children: [
                   const SizedBox(height: Constants.spacerLarge),
-                  Image.asset(
-                      "assets/images/icons/wsd_logo_white_transparent.png",
-                      scale: Constants.drawerLogoScale,
-                      fit: BoxFit.fitWidth),
+                  Image.asset("assets/images/icons/wsd_logo_white_transparent.png",
+                      scale: Constants.drawerLogoScale, fit: BoxFit.fitWidth),
                   const SizedBox(height: Constants.bottomMargin),
+                  KompasText(style: AppTextStyles.headerH3.copyWith(color: AppColors.primaryWhite), text: "kompas.sds"),
                   KompasText(
-                      style: AppTextStyles.headerH3
-                          .copyWith(color: AppColors.primaryWhite),
-                      text: "kompas.sds"),
-                  KompasText(
-                      style: AppTextStyles.paragraphText
-                          .copyWith(color: AppColors.primaryWhite),
+                      style: AppTextStyles.paragraphText.copyWith(color: AppColors.primaryWhite),
                       text: "wersja: 0.1.0 (alpha)"),
                   const SizedBox(height: Constants.bottomMargin),
                 ],
               ),
-              //TODO: once the rating becomes available insert rate button here
-              // Spacer(),
-              // Padding(
-              //   padding: const EdgeInsets.all(Constants.minMargin),
-              //   child: TextButton(
-              //     child: Text("Oceń nas"),
-              //     style: ButtonStyle(
-              //         shape:
-              //             MaterialStateProperty.all<RoundedRectangleBorder>(
-              //                 RoundedRectangleBorder(
-              //       borderRadius:
-              //           BorderRadius.circular(Constants.borderRadius),
-              //     ))),
-              //     onPressed: () {
-              //       print('will open app rating system in the future here');
-              //     },
-              //   ),
-              // ),
+              Spacer(),
+              Padding(
+                padding: const EdgeInsets.all(Constants.minMargin),
+                child: TextButton(
+                  child: Text("Oceń nas"),
+                  style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(Constants.borderRadius),
+                  ))),
+                  onPressed: () {
+                    launch('https://play.google.com/store/apps/details?id=pl.sds.kompas');
+                  },
+                ),
+              ),
               Spacer(),
               KompasText(
-                  style: AppTextStyles.paragraphSubtext
-                      .copyWith(color: AppColors.primaryWhite),
+                  style: AppTextStyles.paragraphSubtext.copyWith(color: AppColors.primaryWhite),
                   textAlign: TextAlign.center,
-                  text:
-                      "© $yearToday Wyższe Seminarium Duchowne Salwatorianów w Bagnie"),
+                  text: "© $yearToday Wyższe Seminarium Duchowne Salwatorianów w Bagnie"),
               const SizedBox(height: Constants.bottomMargin),
               Container(
-                margin:
-                    EdgeInsets.symmetric(horizontal: Constants.insideMargin),
+                margin: EdgeInsets.symmetric(horizontal: Constants.insideMargin),
                 child: RichText(
                   text: TextSpan(
                     children: [
                       TextSpan(
                         text: 'www.wsd.sds.pl',
-                        style: TextStyle(
-                            color: AppColors.primaryWhite,
-                            decoration: TextDecoration.underline),
+                        style: TextStyle(color: AppColors.primaryWhite, decoration: TextDecoration.underline),
                         recognizer: TapGestureRecognizer()
                           ..onTap = () {
                             launch('https://wsd.sds.pl/');
