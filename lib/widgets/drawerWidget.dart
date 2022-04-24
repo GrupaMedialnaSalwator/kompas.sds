69c92f45a25de9837328a80dc35f9d722788e585
Łukasz Darowski <lukasz.darowski@gmail.com>
2022-04-24 22:32:11 +0200

    fix drawer dialog

--- lib/widgets/drawerWidget.dart
+++ lib/widgets/drawerWidget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:kompas/statics/colors.dart';
 import 'package:kompas/statics/constants.dart';
+import 'package:kompas/statics/kompasText.dart';
 import 'package:kompas/statics/text_styles.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -50,82 +51,97 @@ class DrawerWidget extends StatelessWidget {
               ),
               onTap: () {
                 print('dialog');
-                Get.defaultDialog(
-                  title: 'Dziękujemy',
-                  titleStyle: AppTextStyles.headerH6,
-                  middleText: 'Jak chcesz nas ocenić?',
-                  middleTextStyle: AppTextStyles.paragraphSubtext,
-                  radius: 8,
-                  backgroundColor: AppColors.primaryWhite,
-                  actions: [
-                    OutlinedButton(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                showDialog(
+                  context: context,
+                  builder: (context) => SimpleDialog(
+                    backgroundColor: AppColors.primaryWhite,
+                    contentPadding: EdgeInsets.all(Constants.bottomMargin),
+                    title: Center(
+                      child: Column(
                         children: [
-                          Icon(
-                            Icons.star,
-                            color: Colors.yellow,
-                          ),
-                          Spacer(flex: 1),
                           Text(
-                            "Publicznie",
-                            style: AppTextStyles.headerH6.copyWith(color: AppColors.primaryWhite),
+                            "Dziękujemy",
+                            style: AppTextStyles.headerH6.copyWith(color: AppColors.primaryDark),
                           ),
-                          Spacer(flex: 1),
+                          Text(
+                            "Jak chcesz nas ocenić?",
+                            style: AppTextStyles.paragraphSubtext.copyWith(color: AppColors.primaryDark),
+                          ),
                         ],
                       ),
-                      style: TextButton.styleFrom(
-                        primary: AppColors.primaryWhite,
-                        backgroundColor: AppColors.primaryNormal,
-                      ),
-                      onPressed: () {
-                        launch('https://play.google.com/store/apps/details?id=pl.sds.kompas')
-                            .whenComplete(() => Get.back());
-                      },
                     ),
-                    OutlinedButton(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(Icons.mail_rounded),
-                          Spacer(flex: 1),
-                          Text(
-                            "Prywatnie",
-                            style: AppTextStyles.headerH6.copyWith(color: AppColors.primaryWhite),
-                          ),
-                          Spacer(flex: 1),
-                        ],
+                    children: [
+                      OutlinedButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(
+                              Icons.star,
+                              color: Colors.yellow,
+                            ),
+                            Spacer(flex: 1),
+                            KompasText(
+                              text: "Publicznie",
+                              style: AppTextStyles.headerH6.copyWith(color: AppColors.primaryWhite),
+                            ),
+                            Spacer(flex: 1),
+                          ],
+                        ),
+                        style: TextButton.styleFrom(
+                          primary: AppColors.primaryWhite,
+                          backgroundColor: AppColors.primaryNormal,
+                        ),
+                        onPressed: () {
+                          launch('https://play.google.com/store/apps/details?id=pl.sds.kompas')
+                              .whenComplete(() => Get.back());
+                        },
                       ),
-                      style: TextButton.styleFrom(
-                        primary: AppColors.primaryWhite,
-                        backgroundColor: AppColors.primaryNormal,
+                      const SizedBox(height: Constants.cardMargin),
+                      OutlinedButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.mail_rounded),
+                            Spacer(flex: 1),
+                            KompasText(
+                              text: "Prywatnie",
+                              style: AppTextStyles.headerH6.copyWith(color: AppColors.primaryWhite),
+                            ),
+                            Spacer(flex: 1),
+                          ],
+                        ),
+                        style: TextButton.styleFrom(
+                          primary: AppColors.primaryWhite,
+                          backgroundColor: AppColors.primaryNormal,
+                        ),
+                        onPressed: () {
+                          launch('https://kompas.sds.pl/#kontakt').whenComplete(() => Get.back());
+                        },
                       ),
-                      onPressed: () {
-                        launch('https://kompas.sds.pl/#kontakt').whenComplete(() => Get.back());
-                      },
-                    ),
-                    OutlinedButton(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(Icons.cancel_rounded),
-                          Spacer(flex: 1),
-                          Text(
-                            "Nie teraz",
-                            style: AppTextStyles.headerH6.copyWith(color: AppColors.primaryWhite),
-                          ),
-                          Spacer(flex: 1),
-                        ],
+                      const SizedBox(height: Constants.cardMargin),
+                      OutlinedButton(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(Icons.cancel_rounded),
+                            Spacer(flex: 1),
+                            KompasText(
+                              text: "Nie teraz",
+                              style: AppTextStyles.headerH6.copyWith(color: AppColors.primaryWhite),
+                            ),
+                            Spacer(flex: 1),
+                          ],
+                        ),
+                        style: TextButton.styleFrom(
+                          primary: AppColors.primaryWhite,
+                          backgroundColor: AppColors.primaryNormal,
+                        ),
+                        onPressed: () {
+                          Get.back();
+                        },
                       ),
-                      style: TextButton.styleFrom(
-                        primary: AppColors.primaryWhite,
-                        backgroundColor: AppColors.primaryNormal,
-                      ),
-                      onPressed: () {
-                        Get.back();
-                      },
-                    ),
-                  ],
+                    ],
+                  ),
                 );
               },
             ),
